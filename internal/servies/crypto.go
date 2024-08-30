c5f82c23f1e52c83d49c37453b6fc1775d91e78a
osamikoyo <v.pylayev@mail.ru>
2024-08-30 13:47:39 +0500

    login

--- internal/servies/crypto.go
+++ internal/servies/crypto.go
@@ -1,1 +1,13 @@
 package servies
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"time"
+)
+
+func GeterateJWTkey() string{
+	uniqueInput := fmt.Sprintf("%s-%d", "some_unique_value", time.Now().UnixNano())
+	hash := sha256.Sum256([]byte(uniqueInput))
+	return fmt.Sprintf("%x", hash)
+}
