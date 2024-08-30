51361eb5930f9b487b0229f1f11923f16c74698d
osamikoyo <v.pylayev@mail.ru>
2024-08-30 10:57:35 +0500

    structs

--- internal/database/register.go
+++ internal/database/register.go
@@ -1,1 +1,18 @@
 package database
+
+type User struct{
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Email 	 string `json:"email"`
+}
+type Chat struct{
+	FirstUser string
+	SecondUser string
+	Data []Message
+}
+type Message struct{
+	Date string
+	FromUser string
+	ToUser string
+	Message string
+}
